16c00052332be1067259c5112ef6e6e990f91cba
Christian Kessler IV <me@cgk.io>
2015-03-23 17:51:23 -0400

    commit

--- app/js/loadDetails.js
+++ app/js/loadDetails.js
@@ -2,11 +2,15 @@
 
 // console.log(flights);
 
+// LOOK @ FOR CODE: https://github.com/AirportLabs/kiosk-engine/blob/master/app.js
+
 function loadDetails(IATA,flightNumber) {
 
   $("#contentarea").load("ajax/details.html", function() {
 
+    var content = flights[IATA];
 
+    content.
 
   });
 
